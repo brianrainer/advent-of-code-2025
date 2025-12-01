3e4a71e011da363cbf46f86bb3f70a91b38f6119
Brian Rainer <brianbulletworks@gmail.com>
2025-12-01 22:05:29 +0700

    add solution for day 1 of 2025 advent of code

---
+++ 1B.py
@@ -0,0 +1,19 @@
+with open("1A.txt", "r") as file:
+    lines = file.readlines()
+
+    ans = 0
+    current = 50
+    for line in lines:
+        direction = line[0]
+        turn = int(line[1:])
+
+        for _ in range(turn):
+            if direction == 'L':
+                current -= 1
+            else:
+                current += 1
+
+            current %= 100
+            if current == 0:
+                ans += 1
+    print(ans)
