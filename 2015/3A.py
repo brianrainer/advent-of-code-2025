8009e62d28cdd24a48512c03ca57fd4f28c731b0
Brian Rainer <brianbulletworks@gmail.com>
2025-12-06 00:45:50 +0700

    add 2015 aoc day 3 solution

---
+++ 2015/3A.py
@@ -0,0 +1,31 @@
+with open("3A.txt", "r") as file:
+    line = file.readline()
+
+    x, y = 0, 0
+    rx, ry = 0, 0
+
+    vis = set()
+    vis.add((x,y))
+
+    is_santa = True
+    for ch in line:
+        dx, dy = 0, 0
+        if ch == '^':
+            dx = -1
+        elif ch == 'v':
+            dx = 1
+        elif ch == '<':
+            dy = -1
+        elif ch == '>':
+            dy = 1
+
+        if is_santa:
+            x += dx
+            y += dy
+            vis.add((x,y))
+        else:
+            rx += dx
+            ry += dy
+            vis.add((rx,ry))
+        is_santa = not is_santa
+    print(len(vis))
