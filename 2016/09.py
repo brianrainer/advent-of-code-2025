252a45b15b3fc01bd2a181f78467c23a9c4c326a
Brian Rainer <brianbulletworks@gmail.com>
2025-12-12 00:42:07 +0700

    Add 2016 aoc day 9 part 2 solution

--- 2016/09.py
+++ 2016/09.py
@@ -4,10 +4,35 @@ def get_lines(filename: str) -> list[str]:
         return [line.rstrip() for line in lines]
 
 
+def decompress_data(s: str) -> int:
+    has_d = False
+    for ch in s:
+        if ch == "(":
+            has_d = True
+    if not has_d:
+        return len(s)
+
+    n = len(s)
+    i = 0
+    res = 0
+    while i < len(s):
+        if s[i] == "(":
+            j = i + 1
+            while j < n and s[j] != ")":
+                j += 1
+            rep_len, rep_count = [int(x) for x in s[i + 1 : j].split("x")]
+            dec_len = decompress_data(s[j + 1 : j + 1 + rep_len])
+            res += dec_len * rep_count
+            i = j + rep_len + 1
+            continue
+        res += 1
+        i += 1
+    return res
+
+
 def parse_data(data: str) -> int:
     n = len(data)
     i = 0
-
     res = 0
     while i < n:
         if data[i] == "(":
@@ -18,16 +43,9 @@ def parse_data(data: str) -> int:
             rep_len, rep_cnt = [int(x) for x in rep.split("x")]
             i = j + rep_len + 1
             res += rep_len * rep_cnt
-            # print('"', end="")
-            # for k in range(j + 1, min(n, i)):
-            #     print(data[k], end="")
-            # print('"', rep_cnt, sep="", end=" ")
             continue
-        # print(data[i], end=" ")
         res += 1
         i += 1
-
-    print(res)
     return res
 
 
@@ -35,13 +53,14 @@ def example():
     fname = "input/2016/09E.txt"
     lines = get_lines(fname)
     for data in lines:
-        parse_data(data)
+        print(decompress_data(data))
 
 
 def main():
     fname = "input/2016/09.txt"
     data = get_lines(fname)[0]
-    parse_data(data)
+    print(parse_data(data))
+    print(decompress_data(data))
 
 
 example()
